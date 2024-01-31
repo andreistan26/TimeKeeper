a81d498e07a4877d2ed2f2826879d3d4e58bd32a
Andrei Stan <andreistan2003@gmail.com>
2024-01-31 19:37:28 +0200

    add server cmd

--- cmd/server.go
+++ cmd/server.go
@@ -1,12 +1,16 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"github.com/andreistan26/TimeKeeper/server"
+	"github.com/spf13/cobra"
+)
 
 func CreateServerCommand() *cobra.Command {
 	command := &cobra.Command{
 		Use:  "server",
 		Long: "Starts the server",
 		RunE: func(cmd *cobra.Command, args []string) error {
+			server.StartServer(cmd.Context())
 			return nil
 		},
 	}
--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"context"
 	"fmt"
-	"log"
 	"os"
 
 	"github.com/andreistan26/TimeKeeper/cmd"
@@ -11,18 +10,18 @@ import (
 )
 
 var rootCmd = &cobra.Command{
-	Use:   "timekeeper",
+	Use: "timekeeper",
 	RunE: func(cmd *cobra.Command, args []string) error {
 		ctx := context.Background()
-		rootCmd.SetContext(ctx)
+		cmd.SetContext(ctx)
 		return nil
-	};
+	},
 }
 
 func main() {
 	rootCmd.AddCommand(cmd.CreateServerCommand())
 	if err := rootCmd.Execute(); err != nil {
-		fmt.Fprintf(os.Stderr, err)
+		fmt.Fprintf(os.Stderr, "%v", err)
 		os.Exit(1)
 	}
 }
